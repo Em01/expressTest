0fa53445be5e49bc5f8ec32486cf2bca61724d16
Emma Williams <github@em01.co.uk>
2018-04-22 16:13:44 +0100

    Initial setup

--- index.js
+++ index.js
@@ -1,1 +1,16 @@
 const express = require('express');
+
+/*
+generates a new application by running a new express app. Used to set up configuration
+that will llisten for incoming requests that are being routed to the express side of
+the app from the node side and then route the requests on to different handlers.
+The route handlers will be associated with the app object.
+*/
+
+const app = express();
+
+app.get('/', (req, res) => {
+  res.send({ hi: 'there' });
+})
+
+app.listen(5000);
