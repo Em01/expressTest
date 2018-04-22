2f7338240ac2afcda839b639c32bf9d0db5c84cf
Emma Williams <github@em01.co.uk>
2018-04-22 16:30:47 +0100

    Further setup

--- index.js
+++ index.js
@@ -1,7 +1,8 @@
 const express = require('express');
 
 /*
-generates a new application by running a new express app. Used to set up configuration
+generates a new application by running a new express app. Represents the underlying express
+server. Used to set up configuration.
 that will llisten for incoming requests that are being routed to the express side of
 the app from the node side and then route the requests on to different handlers.
 The route handlers will be associated with the app object.
@@ -9,8 +10,12 @@ The route handlers will be associated with the app object.
 
 const app = express();
 
+
+//Route handler
 app.get('/', (req, res) => {
   res.send({ hi: 'there' });
 })
 
+
+//express tells node it wants to listen for incoming traffic on port 5000
 app.listen(5000);
