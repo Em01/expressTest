7519faf36f7c6397601519f71ceb38742480df65
Emma Williams <github@em01.co.uk>
2018-04-26 19:58:25 +0100

    setup Passport

--- index.js
+++ index.js
@@ -1,23 +1,9 @@
 const express = require('express');
-
-/*
-generates a new application by running a new express app. Represents the underlying express
-server. Used to set up configuration.
-that will llisten for incoming requests that are being routed to the express side of
-the app from the node side and then route the requests on to different handlers.
-The route handlers will be associated with the app object.
-*/
-
+const passport = require('passport');
+const GoogleStrategy = require('passport-google-oauth20').Strategy;
 const app = express();
 
+passport.use(new GoogleStrategy());
 
-//Route handler
-app.get('/', (req, res) => {
-  res.send({ hi: 'there' });
-})
-
-
-//express tells node it wants to listen for incoming traffic on port 5000 or env var port. dev/prod etc
 const PORT = process.env.PORT || 5000;
-
 app.listen(PORT);
